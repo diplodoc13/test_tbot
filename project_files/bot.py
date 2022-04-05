8f80f986895fcad58328defd063f99e4a70e6756
diplodoc13 <5286606m@gmail.com>
2022-04-05 14:03:06 +0700

    v.00

--- project_files/bot.py
+++ project_files/bot.py
@@ -1,27 +1,14 @@
 import os, sys
+from config import TOKEN
 
 activate_this = "/Users/User/PycharmProjects/test_tbot/venv/Scripts/activate_this.py"  ## Local
 # activate_this = '/home/bot/python3.10_projects/telegram_bots/bot_1_test/venv_bot_1_test/bin/activate_this.py'   ##Server
 with open(activate_this) as f:
     exec(f.read(), {'__file__': activate_this})
 
-import telebot
-from config import TOKEN
+from aiogram import Bot, dispatcher
 
-bot = telebot.TeleBot(TOKEN)
+bot = Bot(token=TOKEN)
+dp = dispatcher(bot)
 
-
-@bot.message_handler(commands=["start"])
-def start_message(message):
-    answer_message = f"Hello, <b>{message.from_user.first_name}</b>"
-    bot.send_message(message.chat.id, answer_message, parse_mode="html")
-
-
-@bot.message_handler()
-def send_photo(message):
-    if message.text == "photo":
-        sending_photo = open("../project_imgs/2.png", "rb")
-        bot.send_photo(message.chat.id, sending_photo)
-
-
-bot.polling(none_stop=True, interval=0)
+@dp.message_handler()
